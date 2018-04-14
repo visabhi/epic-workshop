a8944ec64f7448c1da3cebe226b09bd0ae48ab9e
visabhi <vilasetty@gmail.com>
2018-04-14 13:45:14 -0700

    epic-workshop

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+// alert($("#goal-list").text());
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#new-goal").val("");
+  })
+})
+})
